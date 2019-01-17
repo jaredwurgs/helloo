bd5beecbffd5862dc18e0d84eb31687c3b3b2c7d
jaredwurgs <42579255+jaredwurgs@users.noreply.github.com>
2019-01-17 12:43:38 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jared Wurgler
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jared Wurgler!\n");
+
+  return 0;
+}
